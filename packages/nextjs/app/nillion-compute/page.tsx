3d809f65edbdab90d935e16b3ff1428e1b62094b
You Song Hou <ysongweb3@gmail.com>
2024-04-30 01:52:42 -0400

    feat: add a compare integers Nada program

--- packages/nextjs/app/nillion-compute/page.tsx
+++ packages/nextjs/app/nillion-compute/page.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
   const [nillion, setNillion] = useState<any>(null);
   const [nillionClient, setNillionClient] = useState<any>(null);
 
-  const [programName] = useState<string>("subtraction_simple");
+  const [programName] = useState<string>("hangman");
   const [programId, setProgramId] = useState<string | null>(null);
   const [computeResult, setComputeResult] = useState<string | null>(null);
 
