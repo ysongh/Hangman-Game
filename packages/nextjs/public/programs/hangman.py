d99bfad165bc1cbd37d310c0432bf2c064467758
You Song Hou <ysongweb3@gmail.com>
2024-05-01 20:41:33 -0400

    fix: update the compare integers Nada program

--- packages/nextjs/public/programs/hangman.py
+++ packages/nextjs/public/programs/hangman.py
@@ -5,9 +5,9 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = Integer(0)
-
     if my_int1 == my_int2:
-        new_int = Integer(1)
+        new_int = my_int1 + my_int2 + Integer(100)
+    else:
+        new_int = my_int1 + my_int2
 
     return [Output(new_int, "my_output", party1)]
