530aa0e8909bfa47cf0df7866bec14a840e6596d
You Song Hou <ysongweb3@gmail.com>
2024-05-02 21:08:47 -0400

    fix: update the compare integers Nada program

--- packages/nillion/next-project-programs/src/hangman.py
+++ packages/nillion/next-project-programs/src/hangman.py
@@ -5,9 +5,6 @@ def nada_main():
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    if my_int1 == my_int2:
-        new_int = my_int1 + my_int2 + Integer(100)
-    else:
-        new_int = my_int1 + my_int2
+    new_int = (my_int1 >= my_int2).if_else(Integer(1), Integer(0))
 
     return [Output(new_int, "my_output", party1)]
